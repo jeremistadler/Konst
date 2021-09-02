eeca75ecefacb3a60f8dda7a6425440cd1acc182
Jeremi Stadler <stadler.jeremi@gmail.com>
2021-09-02 19:28:10 +0200

    Update gunnel pre funeral

--- _next/static/YKjzsr2Po7Di_0tNcvtwh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2a32a77d1146aaee87bf.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/mhw0mcuI6OZdxKHni7W83/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/818-ba713e71a9dac4afd6a7.js","static/chunks/pages/index-546c253e17f9aa7c26f5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
