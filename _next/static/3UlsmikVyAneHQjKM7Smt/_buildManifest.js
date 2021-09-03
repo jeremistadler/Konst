795760773443a4b472d7ac319d6e303095f6dbc8
Jeremi Stadler <stadler.jeremi@gmail.com>
2021-09-03 14:25:33 +0200

    Change link

--- _next/static/3UlsmikVyAneHQjKM7Smt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/818-ba713e71a9dac4afd6a7.js","static/chunks/pages/index-f87b883fddc9a28fe4b1.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
